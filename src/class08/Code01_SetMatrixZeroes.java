bd217ac4f5ded56880eeee784939ec287ee0498a
左程云 <zuochengyun@zuochengyundeMacBook-Pro.local>
2020-06-04 03:45:10 +0800

    add less space solution

--- src/class08/Code01_SetMatrixZeroes.java
+++ src/class08/Code01_SetMatrixZeroes.java
@@ -7,45 +7,75 @@ public class Code01_SetMatrixZeroes {
 	 * 
 	 */
 
-	public static void setZeroes(int[][] matrix) {
+	public static void setZeroes1(int[][] matrix) {
 		boolean row0Zero = false;
 		boolean col0Zero = false;
-		int N = matrix.length;
-		int M = matrix[0].length;
-		for (int i = 0; i < M; i++) {
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < matrix[0].length; i++) {
 			if (matrix[0][i] == 0) {
 				row0Zero = true;
 				break;
 			}
 		}
-		for (int i = 0; i < N; i++) {
+		for (i = 0; i < matrix.length; i++) {
 			if (matrix[i][0] == 0) {
 				col0Zero = true;
 				break;
 			}
 		}
-		for (int i = 1; i < N; i++) {
-			for (int j = 1; j < M; j++) {
+		for (i = 1; i < matrix.length; i++) {
+			for (j = 1; j < matrix[0].length; j++) {
 				if (matrix[i][j] == 0) {
 					matrix[i][0] = 0;
 					matrix[0][j] = 0;
 				}
 			}
 		}
-		for (int i = 1; i < N; i++) {
-			for (int j = 1; j < M; j++) {
+		for (i = 1; i < matrix.length; i++) {
+			for (j = 1; j < matrix[0].length; j++) {
 				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
 					matrix[i][j] = 0;
 				}
 			}
 		}
 		if (row0Zero) {
-			for (int i = 0; i < M; i++) {
+			for (i = 0; i < matrix[0].length; i++) {
 				matrix[0][i] = 0;
 			}
 		}
 		if (col0Zero) {
-			for (int i = 0; i < N; i++) {
+			for (i = 0; i < matrix.length; i++) {
+				matrix[i][0] = 0;
+			}
+		}
+	}
+
+	public static void setZeroes2(int[][] matrix) {
+		boolean col0 = false;
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < matrix.length; i++) {
+			for (j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] == 0) {
+					matrix[i][0] = 0;
+					if (j == 0) {
+						col0 = true;
+					} else {
+						matrix[0][j] = 0;
+					}
+				}
+			}
+		}
+		for (i = matrix.length - 1; i >= 0; i--) {
+			for (j = 1; j < matrix[0].length; j++) {
+				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+		if (col0) {
+			for (i = 0; i < matrix.length; i++) {
 				matrix[i][0] = 0;
 			}
 		}
