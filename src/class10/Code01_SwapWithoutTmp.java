bb1bd7703ca21b10bc1da5080954a2fe8b059e42
左程云 <zuochengyun@zuochengyundeMacBook-Pro.local>
2020-06-17 01:31:47 +0800

    add public class 10

---
+++ src/class10/Code01_SwapWithoutTmp.java
@@ -0,0 +1,17 @@
+package class10;
+
+public class Code01_SwapWithoutTmp {
+
+	public static void main(String[] args) {
+		int a = 16;
+		int b = 111;
+		System.out.println(a);
+		System.out.println(b);
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
