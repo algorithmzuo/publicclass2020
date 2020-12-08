88f7c2b2abc64e6b466844f84f19f3f3745b05c8
左程云 <algorithmzuo@163.com>
2020-12-08 16:03:31 +0800

    add public class 27

--- src/class27/Code02_CountFiles.java
+++ src/class27/Code02_CountFiles.java
@@ -7,7 +7,6 @@ public class Code02_CountFiles {
 
 	// 注意这个函数也会统计隐藏文件
 	public static int getFileNumber(String folderPath) {
-		// File   (文件夹、文件)
 		File root = new File(folderPath);
 		if (!root.isDirectory() && !root.isFile()) {
 			return 0;
@@ -15,7 +14,8 @@ public class Code02_CountFiles {
 		if (root.isFile()) {
 			return 1;
 		}
-		// File  文件夹  文件   stack只放文件夹
+		// File分为文件夹和文件
+		// 而stack只放文件夹
 		Stack<File> stack = new Stack<>();
 		stack.add(root);
 		int files = 0;
@@ -34,8 +34,8 @@ public class Code02_CountFiles {
 	}
 
 	public static void main(String[] args) {
-		// 你可以自己更改目录
-		String path = "/Users/zuochengyun/Desktop/";
+		// 你可以自己更改目录来查看这个目录下的文件个数
+		String path = "/Users/zuochengyun/Desktop";
 		System.out.println(getFileNumber(path));
 	}
 
