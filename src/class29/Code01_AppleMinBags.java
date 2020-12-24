12c5e4ea76689ff84fd7f85304351e9912eb8443
左程云 <algorithmzuo@163.com>
2020-12-24 14:39:27 +0800

    modify code on class

--- src/class29/Code01_AppleMinBags.java
+++ src/class29/Code01_AppleMinBags.java
@@ -2,13 +2,14 @@ package class29;
 
 public class Code01_AppleMinBags {
 
-	public static int minBag(int apple) {
+	public static int minBag1(int apple) {
 		if (apple < 0) {
 			return -1;
 		}
 		if (apple == 0) {
 			return 0;
 		}
+		// 最多的8号袋开始试，
 		for (int max = (apple / 8); max >= 0; max--) {
 			int rest = apple - (max * 8);
 			if (rest % 6 == 0) {
@@ -16,6 +17,34 @@ public class Code01_AppleMinBags {
 			}
 		}
 		return -1;
+	}
+
+	// 18 ~
+	// 18 ~ 25 0组 奇数 -1 偶数 3
+	// 26 ~ 33 1组 奇数 -1 偶数 4
+	// 34 ~ 41 2组 奇数 -1 偶数 5
+	// X
+	// i组 奇数 -1 偶数 i+3
+
+	// 规律解 通过
+	public static int minBag2(int apple) {
+		if (apple < 18) {
+			if (apple < 0) {
+				return -1;
+			}
+			if (apple == 0) {
+				return 0;
+			}
+			if (apple == 6 || apple == 8) {
+				return 1;
+			}
+			if (apple == 12 || apple == 14 || apple == 16) {
+				return 2;
+			}
+			return -1;
+		}
+		int team = (apple - 18) / 8;
+		return (apple & 1) == 0 ? (team + 3) : -1;
 	}
 
 	public static int minBagAwesome(int apple) {
@@ -39,8 +68,8 @@ public class Code01_AppleMinBags {
 	}
 
 	public static void main(String[] args) {
-		for (int apple = 0; apple < 100; apple++) {
-			System.out.println(minBag(apple));
+		for (int apple = 0; apple <= 100; apple++) {
+			System.out.println(minBag1(apple) == minBag2(apple));
 		}
 
 	}
--- src/class29/Code02_EatGrass.java
+++ src/class29/Code02_EatGrass.java
@@ -2,20 +2,23 @@ package class29;
 
 public class Code02_EatGrass {
 
+	// 当前有N份草，当前轮到先手先吃，然后后手再吃
+	// 1，4，16，64，...
+	// 返回  String   "先手"   "后手"
 	public static String winner1(int n) {
 		if (n < 5) {
 			return (n == 0 || n == 2) ? "后手" : "先手";
 		}
-
-		int base = 1;
-		while (base <= n) {
-			if (winner1(n - base).equals("后手")) {
+		// 先手吃的草数
+		int eat = 1;
+		while (eat <= n) {
+			if (winner1(n - eat).equals("后手")) {
 				return "先手";
 			}
-			if (base > n / 4) {
+			if (eat > n / 4) { // 防止溢出
 				break;
 			}
-			base *= 4;
+			eat *= 4;
 		}
 		return "后手";
 	}
