348eb808133bce7f4a5455595320733e2a07b09f
左程云 <algorithmzuo@163.com>
2021-01-06 17:32:21 +0800

    modify code

--- src/class31/Code02_AddMinus.java
+++ src/class31/Code02_AddMinus.java
@@ -6,14 +6,24 @@ public class Code02_AddMinus {
 		int t = 0;
 		while (b != 0) {
 			t = a;
-			a = a ^ b;
-			b = ((t & b) << 1);
+			a = a ^ b; // a -> a' 无进位相加信息
+			b = ((t & b) << 1); // b -> b' 进位信息
 		}
 		return a;
 	}
 
 	public static int minus(int a, int b) {
 		return add(a, add(~b, 1));
+	}
+
+	public static void main(String[] args) {
+		int a = 8739284;
+		int b = 7348472;
+		System.out.println(a + b);
+		System.out.println(add(a, b));
+
+		System.out.println(a - b);
+		System.out.println(minus(a, b));
 	}
 
 }
--- src/class31/Code04_MissingNumber.java
+++ src/class31/Code04_MissingNumber.java
@@ -3,19 +3,19 @@ package class31;
 // 测试链接：https://leetcode.com/problems/first-missing-positive/
 public class Code04_MissingNumber {
 
-	public static int missingNumber(int[] arr) {
-		int l = 0;
+	public static int firstMissingPositive(int[] arr) {
+		int l = -1;
 		int r = arr.length;
-		while (l < r) {
-			if (arr[l] == l) {
+		while (l + 1 != r) {
+			if (arr[l + 1] == l + 2) {
 				l++;
-			} else if (arr[l] < l || arr[l] >= r || arr[arr[l]] == arr[l]) {
-				swap(arr, l, --r);
+			} else if (arr[l + 1] <= l + 1 || arr[l + 1] > r || arr[arr[l + 1] - 1] == arr[l + 1]) {
+				swap(arr, l + 1, --r);
 			} else {
-				swap(arr, l, arr[l]);
+				swap(arr, l + 1, arr[l + 1] - 1);
 			}
 		}
-		return l;
+		return l + 2;
 	}
 
 	public static void swap(int[] arr, int i, int j) {
