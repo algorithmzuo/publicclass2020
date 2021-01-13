ef9f813a0137d78ea3aea42c27b1a099ca4949cb
左程云 <algorithmzuo@163.com>
2021-01-13 17:06:05 +0800

    modify code

--- src/class32/Code03_PaperFolding.java
+++ src/class32/Code03_PaperFolding.java
@@ -7,11 +7,9 @@ public class Code03_PaperFolding {
 		System.out.println();
 	}
 
-	// 当前你来了一个节点，脑海中想象的！
-	// 这个节点在第i层，一共有N层，N固定不变的
-	// 这个节点如果是凹的话，down = T
-	// 这个节点如果是凸的话，down = F
-	// 函数的功能：中序打印以你想象的节点为头的整棵树！
+	// 假想中的当前节点，在i层，一共N层，
+	// 假想中的当前节点凹还是凸？down决定！down = true 凹 down = false 凸
+	// 打印以假想节点为头的整棵树，中序打印
 	public static void process(int i, int N, boolean down) {
 		if (i > N) {
 			return;
