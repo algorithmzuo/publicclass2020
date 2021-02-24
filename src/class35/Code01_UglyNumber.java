559d8ee8e365994e29d2f435d9d500a8674ce57e
左程云 <algorithmzuo@163.com>
2021-02-24 14:24:14 +0800

    add class 35

---
+++ src/class35/Code01_UglyNumber.java
@@ -0,0 +1,57 @@
+package class35;
+
+// 测试链接：https://leetcode.com/problems/ugly-number-ii
+public class Code01_UglyNumber {
+
+	public static boolean isUgly(int num) {
+		while (num % 2 == 0) {
+			num /= 2;
+		}
+		while (num % 3 == 0) {
+			num /= 3;
+		}
+		while (num % 5 == 0) {
+			num /= 5;
+		}
+		return num == 1;
+	}
+
+	public static int nthUglyNumber1(int n) {
+		int find = 0;
+		int num = 1;
+		for (; find < n; num++) {
+			if (isUgly(num)) {
+				find++;
+			}
+		}
+		return num - 1;
+	}
+
+	public static int nthUglyNumber2(int n) {
+		int[] ugly = new int[n + 1];
+		ugly[1] = 1;
+		int i2 = 1;
+		int i3 = 1;
+		int i5 = 1;
+		for (int i = 2; i <= n; i++) {
+			ugly[i] = Math.min(Math.min(ugly[i2] * 2, ugly[i3] * 3), ugly[i5] * 5);
+			if (ugly[i] == ugly[i2] * 2) {
+				i2++;
+			}
+			if (ugly[i] == ugly[i3] * 3) {
+				i3++;
+			}
+			if (ugly[i] == ugly[i5] * 5) {
+				i5++;
+			}
+		}
+		return ugly[n];
+	}
+	
+	public static void main(String[] args) {
+		for(int  i = 1; i < 100;i++) {
+			System.out.println(nthUglyNumber2(i));
+		}
+	}
+
+}
---
+++ src/class35/Code02_MakeNo.java
@@ -0,0 +1,50 @@
+package class35;
+
+public class Code02_MakeNo {
+
+	public static int[] makeNo(int size) {
+		if (size == 1) {
+			return new int[] { 1 };
+		}
+		int halfSize = (size + 1) / 2;
+		int[] base = makeNo(halfSize);
+		int[] ans = new int[size];
+		int index = 0;
+		for (; index < halfSize; index++) {
+			ans[index] = base[index] * 2 + 1;
+		}
+		for (int i = 0; index < size; index++, i++) {
+			ans[index] = base[i] * 2;
+		}
+		return ans;
+	}
+
+	// 检验函数
+	public static boolean isValid(int[] arr) {
+		int N = arr.length;
+		for (int i = 0; i < N; i++) {
+			for (int k = i + 1; k < N; k++) {
+				for (int j = k + 1; j < N; j++) {
+					if (arr[i] + arr[j] == 2 * arr[k]) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("test begin");
+		for (int N = 1; N < 1000; N++) {
+			int[] arr = makeNo(N);
+			if (!isValid(arr)) {
+				System.out.println("Oops!");
+			}
+		}
+		System.out.println("test end");
+		System.out.println(isValid(makeNo(1042)));
+		System.out.println(isValid(makeNo(2981)));
+	}
+
+}
