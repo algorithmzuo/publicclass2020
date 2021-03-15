68ea29a0918705d30989f7a0aa450ad85aeaae08
左程云 <algorithmzuo@163.com>
2021-03-15 17:43:14 +0800

    modify code

--- src/class37/Code02_JumpGameII.java
+++ src/class37/Code02_JumpGameII.java
@@ -7,17 +7,17 @@ public class Code02_JumpGameII {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
-		int jump = 0;
+		int step = 0;
 		int cur = 0;
 		int next = 0;
 		for (int i = 0; i < arr.length; i++) {
 			if (cur < i) {
-				jump++;
+				step++;
 				cur = next;
 			}
 			next = Math.max(next, i + arr[i]);
 		}
-		return jump;
+		return step;
 	}
 
 }
