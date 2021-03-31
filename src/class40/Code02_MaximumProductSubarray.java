c659083b3154bc8920b233fa9f5e160cbc73515e
algorithmzuo <algorithmzuo@163.com>
2021-03-31 12:39:22 +0800

    add class 40

---
+++ src/class40/Code02_MaximumProductSubarray.java
@@ -0,0 +1,20 @@
+package class40;
+
+// 本次测试链接 : https://leetcode.com/problems/maximum-product-subarray/
+public class Code02_MaximumProductSubarray {
+
+	public static int maxProduct(int[] nums) {
+		int ans = nums[0];
+		int min = nums[0];
+		int max = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			int curmin = Math.min(nums[i], Math.min(min * nums[i], max * nums[i]));
+			int curmax = Math.max(nums[i], Math.max(min * nums[i], max * nums[i]));
+			min = curmin;
+			max = curmax;
+			ans = Math.max(ans, max);
+		}
+		return ans;
+	}
+
+}
