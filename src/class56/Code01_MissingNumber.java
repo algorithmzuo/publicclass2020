bf431f4d08cf9fa4f8e463a4c6e297ee0d73b3de
algorithmzuo <algorithmzuo@163.com>
2021-08-20 12:00:58 +0800

    modify code

--- src/class56/Code01_MissingNumber.java
+++ src/class56/Code01_MissingNumber.java
@@ -4,14 +4,21 @@ package class56;
 public class Code01_MissingNumber {
 
 	public static int firstMissingPositive(int[] arr) {
-		int l = 0;
+		int l = 0; // l的左边，每一个位置i，上面都放着i+1这个数
+		// 最好预期:1~r每个数字收集全，且只收集一次
+		// 垃圾区：r....
 		int r = arr.length;
 		while (l != r) {
 			if (arr[l] == l + 1) {
 				l++;
 			} else if (arr[l] <= l || arr[l] > r || arr[arr[l] - 1] == arr[l]) {
+				// [l] = 83
+				// 83应该放在82位置
+				// arr [ arr[l] - 1 ] = arr[l]
+				// arr[82] = 83
+				// 此时的arr[l]是垃圾
 				swap(arr, l, --r);
-			} else {
+			} else { // l上面的数不是l+1 l上面的数也不是垃圾
 				swap(arr, l, arr[l] - 1);
 			}
 		}
--- src/class56/Code02_RabbitsInForest.java
+++ src/class56/Code02_RabbitsInForest.java
@@ -4,25 +4,27 @@ import java.util.Arrays;
 
 public class Code02_RabbitsInForest {
 
-    public static int numRabbits(int[] arr) {
+	public static int numRabbits(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
 		Arrays.sort(arr);
 		int x = arr[0];
-		int c = 1;
+		int k = 1;
 		int ans = 0;
 		for (int i = 1; i < arr.length; i++) {
 			if (x != arr[i]) {
-				ans += ((c + x) / (x + 1)) * (x + 1);
+				// (k / (x + 1)) 上 * (x+1)
+				// (k + x + 1 - 1) / (x + 1)
+				// (k + x) / (x + 1) (x + 1)
+				ans += ((k + x) / (x + 1)) * (x + 1);
 				x = arr[i];
-				c = 1;
+				k = 1;
 			} else {
-				c++;
+				k++;
 			}
 		}
-		return ans + ((c + x) / (x + 1)) * (x + 1);
+		return ans + ((k + x) / (x + 1)) * (x + 1);
 	}
-	
-	
+
 }
