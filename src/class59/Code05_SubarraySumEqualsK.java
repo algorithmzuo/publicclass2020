6cde3f5d99c504c6f36dd15f340eb950667a620d
algorithmzuo <algorithmzuo@163.com>
2021-09-06 14:33:55 +0800

    modify code

--- src/class59/Code05_SubarraySumEqualsK.java
+++ src/class59/Code05_SubarraySumEqualsK.java
@@ -4,21 +4,24 @@ import java.util.HashMap;
 
 public class Code05_SubarraySumEqualsK {
 
-	public static int subarraySum(int[] nums, int k) {
+	public static int subarraySum(int[] nums, int sum) {
 		if (nums == null || nums.length == 0) {
 			return 0;
 		}
+		// key : 某一个前缀和！
+		// value : 这个前缀和，出现了几次！
 		HashMap<Integer, Integer> preSumTimesMap = new HashMap<>();
-		// 这一句非常重要，表示没有遍历之前你已经可以得到前缀和为0的情况了
-		// 那就是，一个数也没有的时候！
 		preSumTimesMap.put(0, 1);
+		// 每一步的整体和, 当你遍历到i位置，0~i整体的累加和！
 		int all = 0;
 		int ans = 0;
 		for (int i = 0; i < nums.length; i++) {
+			// 0...i的整体累加和了！1000   200   1000 - 200 = 800
 			all += nums[i];
-			if (preSumTimesMap.containsKey(all - k)) {
-				ans += preSumTimesMap.get(all - k);
+			if (preSumTimesMap.containsKey(all - sum)) {
+				ans += preSumTimesMap.get(all - sum);
 			}
+			// 0....i 这个前缀和，一定要去！更新map！
 			if (!preSumTimesMap.containsKey(all)) {
 				preSumTimesMap.put(all, 1);
 			} else {
