87d8a8ce4da550ba0848c69aa0a70cf88ccebad1
algorithmzuo <algorithmzuo@163.com>
2021-10-13 15:27:19 +0800

    modify code

--- src/class63/Code01_SumNoPositiveMinCost.java
+++ src/class63/Code01_SumNoPositiveMinCost.java
@@ -13,62 +13,46 @@ import java.util.Arrays;
 // 数据规模 : 面试时面试官没有说数据规模
 public class Code01_SumNoPositiveMinCost {
 
+	// 暴力尝试
 	public static int minOpStep1(int[] arr, int x, int y) {
-		Arrays.sort(arr); // 小 -> 大
-		int n = arr.length;
-		for (int l = 0, r = n - 1; l <= r; l++, r--) {
-			int tmp = arr[l];
-			arr[l] = arr[r];
-			arr[r] = tmp;
+		int sum = 0;
+		for (int num : arr) {
+			sum += num;
 		}
-		// arr 大 -> 小
-		if (x >= y) { // 没有任何必要执行x操作
-			int sum = 0;
-			for (int num : arr) {
-				sum += num;
-			}
-			int cost = 0;
-			for (int i = 0; i < n && sum > 0; i++) {
-				sum -= arr[i] << 1;
-				cost += y;
-			}
-			return cost;
-		} else {
-			for (int i = n - 2; i >= 0; i--) {
-				arr[i] += arr[i + 1];
-			}
-			int benefit = 0;
-			// 注意，可以不二分，用不回退的方式！
-			// 执行Y操作的数，有0个的时候
-			int left = mostLeft(arr, 0, benefit);
-			int cost = left * x;
-			for (int i = 0; i < n - 1; i++) {
-				// 0..i 这些数，都执行Y
-				benefit += arr[i] - arr[i + 1];
-				left = mostLeft(arr, i + 1, benefit);
-				cost = Math.min(cost, (i + 1) * y + (left - i - 1) * x);
-			}
-			return cost;
-		}
+		return process1(arr, x, y, 0, sum);
 	}
 
-	// arr是后缀和数组， arr[l...]中找到值<=v的最左位置
-	public static int mostLeft(int[] arr, int l, int v) {
-		int r = arr.length - 1;
-		int m = 0;
-		int ans = arr.length;
-		while (l <= r) {
-			m = (l + r) / 2;
-			if (arr[m] <= v) {
-				ans = m;
-				r = m - 1;
-			} else {
-				l = m + 1;
-			}
+	// arr[i...]自由选择，每个位置的数可以执行三种操作中的一种！
+	// 执行变0的操作，x操作，代价 -> x
+	// 执行变相反数的操作，y操作，代价 -> y
+	// 还剩下sum这么多累加和，需要去搞定！
+	// 返回搞定了sum，最低代价是多少？
+	public static int process1(int[] arr, int x, int y, int i, int sum) {
+		if (sum <= 0) {
+			return 0;
 		}
-		return ans;
+		// sum > 0 没搞定
+		if (i == arr.length) {
+			return Integer.MAX_VALUE;
+		}
+		// 第一选择，什么也不干！
+		int p1 = process1(arr, x, y, i + 1, sum);
+		// 第二选择，执行x的操作，变0 x + 后续
+		int p2 = Integer.MAX_VALUE;
+		int next2 = process1(arr, x, y, i + 1, sum - arr[i]);
+		if (next2 != Integer.MAX_VALUE) {
+			p2 = x + next2;
+		}
+		// 第三选择，执行y的操作，变相反数 x + 后续 7 -7 -14
+		int p3 = Integer.MAX_VALUE;
+		int next3 = process1(arr, x, y, i + 1, sum - (arr[i] << 1));
+		if (next3 != Integer.MAX_VALUE) {
+			p3 = y + next3;
+		}
+		return Math.min(p1, Math.min(p2, p3));
 	}
 
+	// 最优解(贪心)
 	public static int minOpStep2(int[] arr, int x, int y) {
 		Arrays.sort(arr);
 		int n = arr.length;
@@ -125,10 +109,10 @@ public class Code01_SumNoPositiveMinCost {
 
 	// 为了测试
 	public static void main(String[] args) {
-		int n = 1000;
-		int v = 500;
-		int c = 50;
-		int testTime = 10000;
+		int n = 14;
+		int v = 20;
+		int c = 6;
+		int testTime = 2000;
 		System.out.println("测试开始");
 		for (int i = 0; i < testTime; i++) {
 			int len = (int) (Math.random() * n);
