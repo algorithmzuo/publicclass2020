4d985076956f7978dbc75e1d330c1e2044c79753
algorithmzuo <algorithmzuo@163.com>
2021-10-13 15:21:32 +0800

    modify code

--- src/class63/Code01_SumNoPositiveMinCost.java
+++ src/class63/Code01_SumNoPositiveMinCost.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 // 数据规模 : 面试时面试官没有说数据规模
 public class Code01_SumNoPositiveMinCost {
 
-	public static int minOpStep(int[] arr, int x, int y) {
+	public static int minOpStep1(int[] arr, int x, int y) {
 		Arrays.sort(arr); // 小 -> 大
 		int n = arr.length;
 		for (int l = 0, r = n - 1; l <= r; l++, r--) {
@@ -67,6 +67,83 @@ public class Code01_SumNoPositiveMinCost {
 			}
 		}
 		return ans;
+	}
+
+	public static int minOpStep2(int[] arr, int x, int y) {
+		Arrays.sort(arr);
+		int n = arr.length;
+		for (int l = 0, r = n - 1; l <= r; l++, r--) {
+			int tmp = arr[l];
+			arr[l] = arr[r];
+			arr[r] = tmp;
+		}
+		if (x >= y) {
+			int sum = 0;
+			for (int num : arr) {
+				sum += num;
+			}
+			int cost = 0;
+			for (int i = 0; i < n && sum > 0; i++) {
+				sum -= arr[i] << 1;
+				cost += y;
+			}
+			return cost;
+		} else {
+			for (int i = n - 2; i >= 0; i--) {
+				arr[i] += arr[i + 1];
+			}
+			int benefit = 0;
+			int cost = n * x;
+			for (int i = 0, r = n; i < r - 1; i++) {
+				benefit += arr[i] - arr[i + 1];
+				while (r - 1 > i && arr[r - 1] <= benefit) {
+					r--;
+				}
+				cost = Math.min(cost, (i + 1) * y + (r - i - 1) * x);
+			}
+			return cost;
+		}
+	}
+
+	// 为了测试
+	public static int[] randomArray(int len, int v) {
+		int[] arr = new int[len];
+		for (int i = 0; i < len; i++) {
+			arr[i] = (int) (Math.random() * v) + 1;
+		}
+		return arr;
+	}
+
+	// 为了测试
+	public static int[] copyArray(int[] arr) {
+		int[] ans = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			ans[i] = arr[i];
+		}
+		return ans;
+	}
+
+	// 为了测试
+	public static void main(String[] args) {
+		int n = 1000;
+		int v = 500;
+		int c = 50;
+		int testTime = 10000;
+		System.out.println("测试开始");
+		for (int i = 0; i < testTime; i++) {
+			int len = (int) (Math.random() * n);
+			int[] arr = randomArray(len, v);
+			int[] arr1 = copyArray(arr);
+			int[] arr2 = copyArray(arr);
+			int x = (int) (Math.random() * c);
+			int y = (int) (Math.random() * c);
+			int ans1 = minOpStep1(arr1, x, y);
+			int ans2 = minOpStep2(arr2, x, y);
+			if (ans1 != ans2) {
+				System.out.println("出错了!");
+			}
+		}
+		System.out.println("测试结束");
 	}
 
 }
