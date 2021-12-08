ddb87c810b0796211f2f214511bf5e7a0c4520a7
algorithmzuo <algorithmzuo@163.com>
2021-12-08 15:26:15 +0800

    modify code

--- src/class070/Code01_FindTheCelebrity.java
+++ src/class070/Code01_FindTheCelebrity.java
@@ -4,16 +4,16 @@ package class070;
 public class Code01_FindTheCelebrity {
 
 	// 提交时不要提交这个函数，因为默认系统会给你这个函数
-	// knows方法，自己不认识自己
+	// knows方法默认自己不认识自己
 	public static boolean knows(int x, int i) {
 		return true;
 	}
 
-	// 只提交下面的方法 0 ~ n-1
+	// 只提交下面的方法
 	public int findCelebrity(int n) {
 		// 谁可能成为明星，谁就是cand
 		int cand = 0;
-		for (int i = 0; i < n; ++i) {
+		for (int i = 1; i < n; i++) {
 			if (knows(cand, i)) {
 				cand = i;
 			}
@@ -22,13 +22,13 @@ public class Code01_FindTheCelebrity {
 		// 下一步就是验证，它到底是不是明星
 		// 1) cand是不是不认识所有的人 cand...（右侧cand都不认识）
 		// 所以，只用验证 ....cand的左侧即可
-		for (int i = 0; i < cand; ++i) {
+		for (int i = 0; i < cand; i++) {
 			if (knows(cand, i)) {
 				return -1;
 			}
 		}
 		// 2) 是不是所有的人都认识cand
-		for (int i = 0; i < n; ++i) {
+		for (int i = 0; i < n; i++) {
 			if (!knows(i, cand)) {
 				return -1;
 			}
