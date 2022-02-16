1382e97b92eb5f2b540e93007e0a2d189fcf865e
algorithmzuo <algorithmzuo@163.com>
2022-02-16 19:47:34 +0800

    modify code

--- src/class076/Code04_InsertDeleteGetRandom.java
+++ src/class076/Code04_InsertDeleteGetRandom.java
@@ -2,6 +2,7 @@ package class076;
 
 import java.util.HashMap;
 
+// 测试链接 : https://leetcode.com/problems/insert-delete-getrandom-o1/
 public class Code04_InsertDeleteGetRandom {
 
 	public class RandomizedSet {
