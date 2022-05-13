e15db84df4e58f5a76d4f9f74282d23ca86d8faf
algorithmzuo <algorithmzuo@163.com>
2022-05-13 19:08:03 +0800

    modify code

--- src/class085/Code01_WhoWin21Balls.java
+++ src/class085/Code01_WhoWin21Balls.java
@@ -8,39 +8,41 @@ package class085;
 // 请问谁有必胜策略
 public class Code01_WhoWin21Balls {
 
-	// balls = 21
-	// ball是奇数
-	public static String win(int balls) {
+	// 球数balls，值一定要是奇数！
+	// 返回String，"甲"，"乙"
+	public static String whoWin(int balls) {
 		return process(0, balls, 0, 0);
 	}
 
-	// 憋递归！
-	// turn 谁的回合！
-	// turn == 0 甲回合
-	// turn == 1 乙回合
-	// rest剩余球的数量
-	// 之前，jiaBalls、yiBalls告诉你！
-	// 当前，根据turn，知道是谁的回合！
-	// 当前，还剩多少球，rest
-	// 返回：谁会赢！
+	// turn，谁的回合，turn = 0 甲回合，turn = 1 乙回合
+	// 还剩多少球，rest
+	// 之前甲已经拿了几个球，jia
+	// 之前乙已经拿了几个球，yi
+	// 之前，甲拿了jia个球，乙拿了yi个球
+	// 当前，是谁的回合，turn告诉你！
+	// 还剩几个球呢？rest告诉你
+	// 返回，最终谁赢！
 	public static String process(int turn, int rest, int jia, int yi) {
 		if (rest == 0) {
-			return (jia & 1) == 0 ? "甲" : "乙";
+			return jia % 2 == 0 ? "甲" : "乙";
 		}
-		// rest > 0, 还剩下球！
-		if (turn == 0) { // 甲的回合！
-			// 甲，自己赢！甲赢！
-			for (int pick = 1; pick <= Math.min(rest, 3); pick++) {
-				// pick 甲当前做的选择
-				if (process(1, rest - pick, jia + pick, yi).equals("甲")) {
+		// rest > 0 还有球剩下
+		if (turn == 0) { // 甲的回合
+			// pick 当前甲要拿几个球
+			for (int pick = 1; pick <= Math.min(3, rest); pick++) {
+				// pick 是甲当前的选择
+				String next = process(1, rest - pick, jia + pick, yi);
+				if(next.equals("甲")) {
 					return "甲";
 				}
 			}
 			return "乙";
-		} else {
-			for (int pick = 1; pick <= Math.min(rest, 3); pick++) {
-				// pick 甲当前做的选择
-				if (process(0, rest - pick, jia, yi + pick).equals("乙")) {
+		} else { // 乙的回合
+			// pick 当前乙要拿几个球
+			for (int pick = 1; pick <= Math.min(3, rest); pick++) {
+				// pick 是乙当前的选择
+				String next = process(0, rest - pick, jia, yi + pick);
+				if(next.equals("乙")) {
 					return "乙";
 				}
 			}
@@ -48,10 +50,50 @@ public class Code01_WhoWin21Balls {
 		}
 	}
 
+//	// balls = 21
+//	// ball是奇数
+//	public static String win(int balls) {
+//		return process(0, balls, 0, 0);
+//	}
+//
+//	// 憋递归！
+//	// turn 谁的回合！
+//	// turn == 0 甲回合
+//	// turn == 1 乙回合
+//	// rest剩余球的数量
+//	// 之前，jiaBalls、yiBalls告诉你！
+//	// 当前，根据turn，知道是谁的回合！
+//	// 当前，还剩多少球，rest
+//	// 返回：谁会赢！
+//	public static String process(int turn, int rest, int jia, int yi) {
+//		if (rest == 0) {
+//			return (jia & 1) == 0 ? "甲" : "乙";
+//		}
+//		// rest > 0, 还剩下球！
+//		if (turn == 0) { // 甲的回合！
+//			// 甲，自己赢！甲赢！
+//			for (int pick = 1; pick <= Math.min(rest, 3); pick++) {
+//				// pick 甲当前做的选择
+//				if (process(1, rest - pick, jia + pick, yi).equals("甲")) {
+//					return "甲";
+//				}
+//			}
+//			return "乙";
+//		} else {
+//			for (int pick = 1; pick <= Math.min(rest, 3); pick++) {
+//				// pick 甲当前做的选择
+//				if (process(0, rest - pick, jia, yi + pick).equals("乙")) {
+//					return "乙";
+//				}
+//			}
+//			return "甲";
+//		}
+//	}
+
 	// 为了测试
 	public static void main(String[] args) {
 		for (int balls = 1; balls <= 500; balls += 2) {
-			System.out.println("球数为 " + balls + " 时 , 赢的是 " + win(balls));
+			System.out.println("球数为 " + balls + " 时 , 赢的是 " + whoWin(balls));
 		}
 	}
 
--- src/class085/Code02_ShortestSubarrayWithSumAtLeastK.java
+++ src/class085/Code02_ShortestSubarrayWithSumAtLeastK.java
@@ -7,6 +7,54 @@ package class085;
 // 本题测试链接 : https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
 public class Code02_ShortestSubarrayWithSumAtLeastK {
 
+//	public static int shortestLenSumNolessK(int[] arr, int k) {
+//		int ans = Integer.MAX_VALUE;
+//		Box box = new Box(arr.length);
+//		box.add(0, -1);
+//		int sum = 0;
+//		for (int i = 0; i < arr.length; i++) {
+//			sum += arr[i];
+//			// sum : 0...i 整体累加和！
+//			int find = sum - k;
+//			// box中 <= find 的最右位置！
+//			int right = box.find(find);
+//			if(right != -2) {
+//				ans = Math.min(ans, i - right);
+//			}
+//			box.add(sum, i);
+//		}
+//		return ans == Integer.MAX_VALUE ? -1 : ans;
+//	}
+//
+//	public static class Box {
+//
+//		public Node[] help;// 搞长一些，n长度
+//		int size;
+//		public Box(int n) {
+//			help = new Node[n];
+//			size = 0;
+//		}
+//
+//		// 0...right sum
+//		// 保持单调性去更新
+//		public void add(int sum, int right) {
+//
+//		}
+//		
+//		// 在box中<=aim的最右位置，返回
+//		// 如果不存在，返回-2
+//		// 二分去找！
+//		public int find(int aim) {
+//			
+//		}
+//
+//	}
+//	
+//	public static class Node{
+//		public int sum;
+//		public int right;
+//	}
+
 	public static int shortestSubarray1(int[] arr, int k) {
 		if (arr == null || arr.length < 1) {
 			return -1;
@@ -58,9 +106,11 @@ public class Code02_ShortestSubarrayWithSumAtLeastK {
 		int l = 0;
 		int r = 0;
 		for (int i = 0; i < N + 1; i++) {
+			// 从尾巴干掉，box里的记录！
 			while (l != r && preSums[dq[r - 1]] >= preSums[i]) {
 				r--;
 			}
+			// 从头部干掉，box里的记录
 			while (l != r && preSums[i] - preSums[dq[l]] >= K) {
 				ans = Math.min(ans, i - dq[l++]);
 			}
