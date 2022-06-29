5f7927fd3bc1a36e2379e9715d2e60f92d7c6ba4
algorithmzuo <algorithmzuo@163.com>
2022-06-29 14:32:41 +0800

    modify code

---
+++ src/class090/Code01_LIS.java
@@ -0,0 +1,35 @@
+package class090;
+
+// 本题测试链接 : https://leetcode.com/problems/longest-increasing-subsequence
+public class Code01_LIS {
+
+	public static int lengthOfLIS(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		int[] ends = new int[arr.length];
+		ends[0] = arr[0];
+		int right = 0;
+		int l = 0;
+		int r = 0;
+		int m = 0;
+		int max = 1;
+		for (int i = 1; i < arr.length; i++) {
+			l = 0;
+			r = right;
+			while (l <= r) {
+				m = (l + r) / 2;
+				if (arr[i] > ends[m]) {
+					l = m + 1;
+				} else {
+					r = m - 1;
+				}
+			}
+			right = Math.max(right, l);
+			ends[l] = arr[i];
+			max = Math.max(max, l + 1);
+		}
+		return max;
+	}
+
+}
