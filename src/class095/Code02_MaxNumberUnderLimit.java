b4f8f9f536487336601ff3023b8e400a34e6d227
algorithmzuo <algorithmzuo@163.com>
2022-08-02 13:47:11 +0800

    modify code

---
+++ src/class095/Code02_MaxNumberUnderLimit.java
@@ -0,0 +1,203 @@
+package class095;
+
+import java.util.Arrays;
+
+// 来自字节
+// 输入:
+// 去重数组arr，里面的数只包含0~9
+// limit，一个数字
+// 返回:
+// 要求比limit小的情况下，能够用arr拼出来的最大数字
+public class Code02_MaxNumberUnderLimit {
+
+	public static int tmp = 0;
+
+	// 暴力尝试的方法
+	public static int maxNumber1(int[] arr, int limit) {
+		tmp = 0;
+		Arrays.sort(arr);
+		limit--;
+		int offset = 1;
+		while (offset <= limit / 10) {
+			offset *= 10;
+		}
+		process1(arr, 0, offset, limit);
+		if (tmp == 0) {
+			int rest = 0;
+			offset /= 10;
+			while (offset > 0) {
+				rest += arr[arr.length - 1] * offset;
+				offset /= 10;
+			}
+			return rest;
+		}
+		return tmp;
+	}
+
+	public static void process1(int[] arr, int num, int offset, int limit) {
+		if (offset == 0) {
+			if (num <= limit) {
+				tmp = Math.max(tmp, num);
+			}
+		} else {
+			for (int cur : arr) {
+				process1(arr, num * 10 + cur, offset / 10, limit);
+			}
+		}
+	}
+
+	// 正式方法
+	public static int maxNumber2(int[] arr, int limit) {
+		// arr里面是不重复的数字，且只包含0~9
+		// 5, 8 , 2
+		// 2, 5, 8
+		Arrays.sort(arr);
+		limit--;
+		// <= limit 且最大的数字
+		// 68886
+		// 10000
+		// 为了取数而设计的！
+		// 457
+		// 100
+		int offset = 1;
+		while (offset <= limit / 10) {
+			offset *= 10;
+		}
+		// arr, 2 5 8
+		// limit--, 试图去追平的数字
+		// offset, 扣出limit中每一位的数字!
+		// 可以使用arr中的数字，期望得到尽可能接近limit的结果，并且位数一样
+		// 能做到，返回的结果就是答案
+		// 如果做不到和limit位数一样！返回-1
+		int ans = process2(arr, limit, offset);
+		if (ans != -1) {
+			return ans;
+		} else { // ans = -1 一定要减少位数，才能做到
+			offset /= 10;
+			int rest = 0;
+			while (offset > 0) {
+				rest += arr[arr.length - 1] * offset;
+				offset /= 10;
+			}
+			return rest;
+		}
+	}
+
+	// 可以选哪些数字，都在arr里，arr是有序的，[2,5,8]
+	// limit : <= limit 且尽量的大！
+	// offset : 为limit服务的，就是为了提取limit的每一位数字
+	public static int process2(int[] arr, int limit, int offset) {
+		// offset 1000 100 10 1 0
+		// offset == 0，意味着，之前的数字，全都可以追平，返回limit
+		// 之前的数字和limit完全一样，且limit所有数字都一样
+		if (offset == 0) {
+			return limit;
+		}
+		// limit中还有数字，没遍历到，
+		// (limit / offset) % 10
+		// 当前数字是谁，提取出来
+		int cur = (limit / offset) % 10;
+		
+		// 2 5 8
+		//  <=4
+		//   2 ....8888
+		int near = near(arr, cur);
+		if (near == -1) {
+			return -1;
+		} else if (arr[near] == cur) { // 找出来的数字，真的和当前数字cur一样!
+			// 当前位，追平了！
+			// 看看后续能不能走的通！
+			int ans = process2(arr, limit, offset / 10);
+			// 1) 走通了！
+			// 2) 走不通，
+			// A ：当前位还有下降的空间！
+			// B ：当前位没有下降的空间！
+			if (ans != -1) {
+				return ans;
+			} else if (near > 0) { // 虽然后续没成功，但是我自己还能下降！还能选更小的数字
+				near--;
+				return (limit / (offset * 10)) * offset * 10 + (arr[near] * offset) + rest(arr, offset / 10);
+			} else { // 后续没成功，我自己也不能再下降了！宣告失败，往上返回！
+				return -1;
+			}
+		} else { // arr[near] < cur
+ 			return (limit / (offset * 10)) * offset * 10 + (arr[near] * offset) + rest(arr, offset / 10);
+		}
+	}
+
+	// 比如offset = 100
+	// 一共3位数
+	// 那么就把arr中最大的数字x，拼成xxx，返回
+	// 比如offset = 10000
+	// 一共5位数
+	// 那么就把arr中最大的数字x，拼成xxxxx，返回
+	public static int rest(int[] arr, int offset) {
+		int rest = 0;
+		while (offset > 0) {
+			rest += arr[arr.length - 1] * offset;
+			offset /= 10;
+		}
+		return rest;
+	}
+
+	// 在有序数组arr中，找到<=num，且最大的数字，在arr中的位置返回
+	// 如果所有数字都大于num，返回-1
+	// [3,6,9] num = 4  3
+	// [5,7,9] num = 4  -1
+	public static int near(int[] arr, int num) {
+		int l = 0;
+		int r = arr.length - 1;
+		int m = 0;
+		int near = -1;
+		while (l <= r) {
+			m = (l + r) / 2;
+			if (arr[m] <= num) {
+				near = m;
+				l = m + 1;
+			} else {
+				r = m - 1;
+			}
+		}
+		return near;
+	}
+
+	// 为了测试
+	public static int[] randomArray() {
+		int[] arr = new int[(int) (Math.random() * 10) + 1];
+		boolean[] cnt = new boolean[10];
+		for (int i = 0; i < arr.length; i++) {
+			do {
+				arr[i] = (int) (Math.random() * 10);
+			} while (cnt[arr[i]]);
+			cnt[arr[i]] = true;
+		}
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		int max = 3000;
+		int testTime = 100;
+		System.out.println("测试开始");
+		for (int i = 0; i < max; i++) {
+			int[] arr = randomArray();
+			for (int j = 0; j < testTime; j++) {
+				int ans1 = maxNumber1(arr, i);
+				int ans2 = maxNumber2(arr, i);
+				if (ans1 != ans2) {
+					System.out.println("出错了!");
+					System.out.println("数组为 ：");
+					for (int num : arr) {
+						System.out.print(num + " ");
+					}
+					System.out.println();
+					System.out.println("数字为 ：" + i);
+					System.out.println(ans1);
+					System.out.println(ans2);
+				}
+			}
+		}
+		System.out.println("测试结束");
+
+	}
+
+}
---
+++ src/class095/Code03_SwimInRisingWater.java
@@ -0,0 +1,155 @@
+package class095;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+// 在一个 n x n 的整数矩阵 grid 中，
+// 每一个方格的值 grid[i][j] 表示位置 (i, j) 的平台高度。
+// 当开始下雨时，在时间为 t 时，水池中的水位为 t 。
+// 你可以从一个平台游向四周相邻的任意一个平台，但是前提是此时水位必须同时淹没这两个平台。
+// 假定你可以瞬间移动无限距离，也就是默认在方格内部游动是不耗时的。
+// 当然，在你游泳的时候你必须待在坐标方格里面。
+// 你从坐标方格的左上平台 (0，0) 出发。
+// 返回 你到达坐标方格的右下平台 (n-1, n-1) 所需的最少时间 。
+// 测试链接 ：https://leetcode.cn/problems/swim-in-rising-water
+public class Code03_SwimInRisingWater {
+
+	// 并查集的解法
+	public static int swimInWater1(int[][] grid) {
+		// 行号
+		int n = grid.length;
+		// 列号
+		int m = grid[0].length;
+		// [0,0,5]
+		// [0,1,3]....
+		int[][] points = new int[n * m][3];
+		int pi = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				points[pi][0] = i;
+				points[pi][1] = j;
+				points[pi++][2] = grid[i][j];
+			}
+		}
+		// 所有格子小对象，生成好了!
+		// 排序！[a,b,c]  [d,e,f]
+		Arrays.sort(points, (a, b) -> a[2] - b[2]);
+		// 生成并查集！n * m
+		// 初始化的时候，把所有格子独自成一个集合！
+		UnionFind uf = new UnionFind(n, m);
+		int ans = 0;
+		for (int i = 0; i < points.length; i++) {
+			int r = points[i][0];
+			int c = points[i][1];
+			int v = points[i][2];
+			if (r > 0 && grid[r - 1][c] <= v) {
+				uf.union(r, c, r - 1, c);
+			}
+			if (r < n - 1 && grid[r + 1][c] <= v) {
+				uf.union(r, c, r + 1, c);
+			}
+			if (c > 0 && grid[r][c - 1] <= v) {
+				uf.union(r, c, r, c - 1);
+			}
+			if (c < m - 1 && grid[r][c + 1] <= v) {
+				uf.union(r, c, r, c + 1);
+			}
+			if (uf.isSameSet(0, 0, n - 1, m - 1)) {
+				ans = v;
+				break;
+			}
+		}
+		return ans;
+	}
+
+	public static class UnionFind {
+		public int col;
+		public int pointsSize;
+		public int[] father;
+		public int[] size;
+		public int[] help;
+
+		public UnionFind(int n, int m) {
+			col = m;
+			pointsSize = n * m;
+			father = new int[pointsSize];
+			size = new int[pointsSize];
+			help = new int[pointsSize];
+			for (int i = 0; i < pointsSize; i++) {
+				father[i] = i;
+				size[i] = 1;
+			}
+		}
+
+		private int find(int i) {
+			int hi = 0;
+			while (i != father[i]) {
+				help[hi++] = i;
+				i = father[i];
+			}
+			while (hi > 0) {
+				father[help[--hi]] = i;
+			}
+			return i;
+		}
+
+		private int index(int i, int j) {
+			return i * col + j;
+		}
+
+		public void union(int row1, int col1, int row2, int col2) {
+			int f1 = find(index(row1, col1));
+			int f2 = find(index(row2, col2));
+			if (f1 != f2) {
+				if (size[f1] >= size[f2]) {
+					father[f2] = f1;
+					size[f1] += size[f2];
+				} else {
+					father[f1] = f2;
+					size[f2] += size[f1];
+				}
+			}
+		}
+
+		public boolean isSameSet(int row1, int col1, int row2, int col2) {
+			return find(index(row1, col1)) == find(index(row2, col2));
+		}
+
+	}
+
+	// Dijkstra算法
+	public static int swimInWater2(int[][] grid) {
+		int n = grid.length;
+		int m = grid[0].length;
+		PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+		boolean[][] visited = new boolean[n][m];
+		heap.add(new int[] { 0, 0, grid[0][0] });
+		int ans = 0;
+		while (!heap.isEmpty()) {
+			int r = heap.peek()[0];
+			int c = heap.peek()[1];
+			int v = heap.peek()[2];
+			heap.poll();
+			if (visited[r][c]) {
+				continue;
+			}
+			visited[r][c] = true;
+			if (r == n - 1 && c == m - 1) {
+				ans = v;
+				break;
+			}
+			add(grid, heap, visited, r - 1, c, v);
+			add(grid, heap, visited, r + 1, c, v);
+			add(grid, heap, visited, r, c - 1, v);
+			add(grid, heap, visited, r, c + 1, v);
+		}
+		return ans;
+	}
+
+	public static void add(int[][] grid, PriorityQueue<int[]> heap, boolean[][] visited, int r, int c, int preV) {
+		if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && !visited[r][c]) {
+			heap.add(new int[] { r, c, preV + Math.max(0, grid[r][c] - preV) });
+		}
+	}
+
+}
