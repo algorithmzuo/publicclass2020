ce7a7d297954616b97e5a794ed99ee350ab7905c
algorithmzuo <algorithmzuo@163.com>
2022-08-09 15:49:00 +0800

    modify code

---
+++ src/class096/Code02_ShortestImpossibleSequenceOfRolls.java
@@ -0,0 +1,37 @@
+package class096;
+
+import java.util.Arrays;
+
+// 给你一个长度为 n 的整数数组 rolls 和一个整数 k 。
+// 你扔一个 k 面的骰子 n 次，骰子的每个面分别是 1 到 k ，
+// 其中第 i 次扔得到的数字是 rolls[i] 。
+// 请你返回 无法 从 rolls 中得到的 最短 骰子子序列的长度。
+// 扔一个 k 面的骰子 len 次得到的是一个长度为 len 的 骰子子序列 。
+// 注意 ，子序列只需要保持在原数组中的顺序，不需要连续。
+// 测试链接 : https://leetcode.cn/problems/shortest-impossible-sequence-of-rolls/
+public class Code02_ShortestImpossibleSequenceOfRolls {
+
+	// 所有数字1~k
+	public static int shortestSequence(int[] rolls, int k) {
+		// 1~k上，某个数字是否收集到了！
+		// set[i] == true
+		// set[i] == false
+		boolean[] set = new boolean[k + 1];
+		// 当前这一套，收集了几个数字了？
+		int size = 0;
+		int ans = 0;
+		for (int num : rolls) {
+			if (!set[num]) {
+				set[num] = true;
+				size++;
+			}
+			if (size == k) {
+				ans++;
+				Arrays.fill(set, false);
+				size = 0;
+			}
+		}
+		return ans + 1;
+	}
+
+}
---
+++ src/class096/Code04_LongestOneLetterManyNumberString.java
@@ -0,0 +1,103 @@
+package class096;
+
+// 给定一个只由小写字母和数字字符组成的字符串str
+// 要求子串必须只含有一个小写字母，数字字符数量随意
+// 求这样的子串最大长度是多少
+public class Code04_LongestOneLetterManyNumberString {
+
+	// 一个绝对正确的暴力方法
+	public static int right(String s) {
+		char[] str = s.toCharArray();
+		int ans = 0;
+		for (int i = 0; i < str.length; i++) {
+			for (int j = i; j < str.length; j++) {
+				if (check(str, i, j)) {
+					ans = Math.max(ans, j - i + 1);
+				}
+			}
+		}
+		return ans;
+	}
+
+	public static boolean check(char[] str, int l, int r) {
+		int letterNumber = 0;
+		for (int i = l; i <= r; i++) {
+			if (str[i] >= 'a' && str[i] <= 'z') {
+				letterNumber++;
+			}
+		}
+		return letterNumber == 1;
+	}
+
+	// 用窗口
+	// 时间复杂度O(N)
+	public static int zuo(String s) {
+		char[] str = s.toCharArray();
+		int n = str.length;
+		// 窗口内有几个小写字母了
+		int letters = 0;
+		// 窗口的右边界
+		// [left, right)
+		int right = 0;
+		int ans = 0;
+		// for枚举了每一个窗口的开始位置，0... 1...... 2.....
+		for (int left = 0; left < n; left++) {
+			while (right < n) { // right不能越界，一旦越界不用再往右了
+				if (letters == 1 && str[right] >= 'a' && str[right] <= 'z') {
+					break;
+				}
+				// letters == 0 str[right]是数字
+				if (str[right] >= 'a' && str[right] <= 'z') {
+					letters++;
+				}
+				right++;
+			}
+			// [left.....right)
+			// [left.....right-1]
+			if (letters == 1) {
+				ans = Math.max(ans, right - left);
+			}
+			if (str[left] >= 'a' && str[left] <= 'z') {
+				letters--;
+			}
+		}
+		return ans;
+	}
+
+	// 为了测试
+	public static char[] chars = { 
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+			'u', 'v', 'w', 'x', 'y', 'z' };
+
+	// 为了测试
+	public static String randomString(int n) {
+		char[] str = new char[n];
+		for (int i = 0; i < n; i++) {
+			str[i] = chars[(int) (Math.random() * chars.length)];
+		}
+		return String.valueOf(str);
+	}
+
+	// 为了测试
+	public static void main(String[] args) {
+		int N = 100;
+		int testTimes = 10000;
+		System.out.println("测试开始");
+		for (int i = 0; i < testTimes; i++) {
+			int n = (int) (Math.random() * N) + 1;
+			String str = randomString(n);
+			int ans1 = right(str);
+			int ans2 = zuo(str);
+			if (ans1 != ans2) {
+				System.out.println(str);
+				System.out.println(ans1);
+				System.out.println(ans2);
+				break;
+			}
+		}
+		System.out.println("测试结束");
+	}
+
+}
