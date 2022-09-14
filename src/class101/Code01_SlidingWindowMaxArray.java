48ef04b59f230b67ef42159e1b16314ed0e109b7
algorithmzuo <algorithmzuo@163.com>
2022-09-14 17:45:36 +0800

    modify code

--- src/class101/Code01_SlidingWindowMaxArray.java
+++ src/class101/Code01_SlidingWindowMaxArray.java
@@ -1,99 +1,177 @@
 package class101;
 
-import java.util.LinkedList;
 
 public class Code01_SlidingWindowMaxArray {
 
-	// 暴力的对数器方法
-	public static int[] right(int[] arr, int w) {
-		if (arr == null || w < 1 || arr.length < w) {
-			return null;
-		}
-		int N = arr.length;
-		int[] res = new int[N - w + 1];
-		int index = 0;
-		int L = 0;
-		int R = w - 1;
-		while (R < N) {
-			int max = arr[L];
-			for (int i = L + 1; i <= R; i++) {
-				max = Math.max(max, arr[i]);
+	public static class Window {
 
-			}
-			res[index++] = max;
-			L++;
-			R++;
-		}
-		return res;
-	}
+		public int[] arr;
 
-	public static int[] getMaxWindow(int[] arr, int w) {
-		if (arr == null || w < 1 || arr.length < w) {
-			return null;
-		}
-		// qmax 窗口最大值的更新结构
-		// 放下标
-		LinkedList<Integer> qmax = new LinkedList<Integer>();
-		int[] res = new int[arr.length - w + 1];
-		int index = 0;
-		for (int R = 0; R < arr.length; R++) {
-			while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[R]) {
-				qmax.pollLast();
-			}
-			qmax.addLast(R);
-			if (qmax.peekFirst() == R - w) {
-				qmax.pollFirst();
-			}
-			if (R >= w - 1) {
-				res[index++] = arr[qmax.peekFirst()];
-			}
-		}
-		return res;
-	}
+		public int[] queue;
+		public int ql;
+		public int qr;
 
-	// for test
-	public static int[] generateRandomArray(int maxSize, int maxValue) {
-		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) (Math.random() * (maxValue + 1));
-		}
-		return arr;
-	}
+		public int l;
+		public int r;
 
-	// for test
-	public static boolean isEqual(int[] arr1, int[] arr2) {
-		if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
-			return false;
+		public Window(int[] a) {
+			arr = a;
+			queue = new int[a.length];
+			l = 0;
+			r = 0;
+			ql = 0;
+			qr = 0;
 		}
-		if (arr1 == null && arr2 == null) {
-			return true;
-		}
-		if (arr1.length != arr2.length) {
-			return false;
-		}
-		for (int i = 0; i < arr1.length; i++) {
-			if (arr1[i] != arr2[i]) {
-				return false;
+
+		// 右边扩了！
+		public void add() {
+			if (r == arr.length) {
+				return;
 			}
+			// [l,r)
+			// 能扩！
+			// arr[r]
+			while (ql != qr && arr[queue[qr - 1]] <= arr[r]) {
+				qr--;
+			}
+			queue[qr++] = r++;
 		}
-		return true;
+
+		// 左边缩了!
+		// 必须有窗口才能缩
+		public void remove() {
+			if (l == r) {
+				return;
+			}
+			if (queue[ql] == l) {
+				ql++;
+			}
+			l++;
+		}
+
+		public int max() {
+			if (l == r) {
+				return -1;
+			}
+			return arr[queue[ql]];
+		}
+
 	}
 
 	public static void main(String[] args) {
-		int testTime = 100000;
-		int maxSize = 100;
-		int maxValue = 100;
-		System.out.println("test begin");
-		for (int i = 0; i < testTime; i++) {
-			int[] arr = generateRandomArray(maxSize, maxValue);
-			int w = (int) (Math.random() * (arr.length + 1));
-			int[] ans1 = getMaxWindow(arr, w);
-			int[] ans2 = right(arr, w);
-			if (!isEqual(ans1, ans2)) {
-				System.out.println("Oops!");
-			}
-		}
-		System.out.println("test finish");
+		int[] arr = { 6, 5, 7, 2, 9, 1, 3 };
+		// l..r 无
+		Window w = new Window(arr);
+
+		w.add();
+		System.out.println(w.max());
+		w.add();
+		System.out.println(w.max());
+		w.add();
+		System.out.println(w.max());
+		w.add();
+		System.out.println(w.max());
+		w.remove();
+		w.remove();
+		w.remove();
+		System.out.println(w.max());
+
 	}
+
+//	
+//	
+//	
+//	
+//	// 暴力的对数器方法
+//	public static int[] right(int[] arr, int w) {
+//		if (arr == null || w < 1 || arr.length < w) {
+//			return null;
+//		}
+//		int N = arr.length;
+//		int[] res = new int[N - w + 1];
+//		int index = 0;
+//		int L = 0;
+//		int R = w - 1;
+//		while (R < N) {
+//			int max = arr[L];
+//			for (int i = L + 1; i <= R; i++) {
+//				max = Math.max(max, arr[i]);
+//
+//			}
+//			res[index++] = max;
+//			L++;
+//			R++;
+//		}
+//		return res;
+//	}
+//
+//	public static int[] getMaxWindow(int[] arr, int w) {
+//		if (arr == null || w < 1 || arr.length < w) {
+//			return null;
+//		}
+//		// qmax 窗口最大值的更新结构
+//		// 放下标
+//		LinkedList<Integer> qmax = new LinkedList<Integer>();
+//		int[] res = new int[arr.length - w + 1];
+//		int index = 0;
+//		for (int R = 0; R < arr.length; R++) {
+//			while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[R]) {
+//				qmax.pollLast();
+//			}
+//			qmax.addLast(R);
+//			if (qmax.peekFirst() == R - w) {
+//				qmax.pollFirst();
+//			}
+//			if (R >= w - 1) {
+//				res[index++] = arr[qmax.peekFirst()];
+//			}
+//		}
+//		return res;
+//	}
+//
+//	// for test
+//	public static int[] generateRandomArray(int maxSize, int maxValue) {
+//		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+//		for (int i = 0; i < arr.length; i++) {
+//			arr[i] = (int) (Math.random() * (maxValue + 1));
+//		}
+//		return arr;
+//	}
+//
+//	// for test
+//	public static boolean isEqual(int[] arr1, int[] arr2) {
+//		if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+//			return false;
+//		}
+//		if (arr1 == null && arr2 == null) {
+//			return true;
+//		}
+//		if (arr1.length != arr2.length) {
+//			return false;
+//		}
+//		for (int i = 0; i < arr1.length; i++) {
+//			if (arr1[i] != arr2[i]) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//
+//	public static void main(String[] args) {
+//		int testTime = 100000;
+//		int maxSize = 100;
+//		int maxValue = 100;
+//		System.out.println("test begin");
+//		for (int i = 0; i < testTime; i++) {
+//			int[] arr = generateRandomArray(maxSize, maxValue);
+//			int w = (int) (Math.random() * (arr.length + 1));
+//			int[] ans1 = getMaxWindow(arr, w);
+//			int[] ans2 = right(arr, w);
+//			if (!isEqual(ans1, ans2)) {
+//				System.out.println("Oops!");
+//			}
+//		}
+//		System.out.println("test finish");
+//	}
 
 }
