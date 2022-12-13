c58c9d124de4977663c24a5ab975fa5233a7fe4b
algorithmzuo <algorithmzuo@163.com>
2022-12-13 15:37:15 +0800

    modify code

---
+++ src/class113/Code02_MakingALargeIsland.java
@@ -0,0 +1,75 @@
+package class113;
+
+import java.util.ArrayList;
+
+// 来自亚马逊、谷歌、微软、Facebook、Bloomberg
+// 给你一个大小为 n x n 二进制矩阵 grid 。最多 只能将一格 0 变成 1 。
+// 返回执行此操作后，grid 中最大的岛屿面积是多少？
+// 岛屿 由一组上、下、左、右四个方向相连的 1 形成。
+// 测试链接 : https://leetcode.cn/problems/making-a-large-island/
+public class Code02_MakingALargeIsland {
+
+	public static int largestIsland(int[][] grid) {
+		int n = grid.length;
+		int m = grid[0].length;
+		ArrayList<Integer> sizes = new ArrayList<>();
+		sizes.add(0);
+		sizes.add(0);
+		int id = 2;
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (grid[i][j] == 1) {
+					int curSize = infect(grid, i, j, id, n, m);
+					ans = Math.max(ans, curSize);
+					sizes.add(id++, curSize);
+				}
+			}
+		}
+		boolean[] visited = new boolean[id];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (grid[i][j] == 0) {
+					int up = i - 1 >= 0 ? grid[i - 1][j] : 0;
+					int down = i + 1 < n ? grid[i + 1][j] : 0;
+					int left = j - 1 >= 0 ? grid[i][j - 1] : 0;
+					int right = j + 1 < m ? grid[i][j + 1] : 0;
+					int merge = 1 + sizes.get(up);
+					visited[up] = true;
+					if (!visited[down]) {
+						merge += sizes.get(down);
+						visited[down] = true;
+					}
+					if (!visited[left]) {
+						merge += sizes.get(left);
+						visited[left] = true;
+					}
+					if (!visited[right]) {
+						merge += sizes.get(right);
+						visited[right] = true;
+					}
+					ans = Math.max(ans, merge);
+					visited[up] = false;
+					visited[down] = false;
+					visited[left] = false;
+					visited[right] = false;
+				}
+			}
+		}
+		return ans;
+	}
+
+	public static int infect(int[][] grid, int i, int j, int v, int n, int m) {
+		if (i < 0 || i == n || j < 0 || j == m || grid[i][j] != 1) {
+			return 0;
+		}
+		int ans = 1;
+		grid[i][j] = v;
+		ans += infect(grid, i - 1, j, v, n, m);
+		ans += infect(grid, i + 1, j, v, n, m);
+		ans += infect(grid, i, j - 1, v, n, m);
+		ans += infect(grid, i, j + 1, v, n, m);
+		return ans;
+	}
+
+}
---
+++ src/class113/Code03_MinimumCostToHireKWorkers.java
@@ -0,0 +1,54 @@
+package class113;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+// 来自亚马逊
+// 有 n 名工人。 给定两个数组 quality 和 wage ，
+// 其中quality[i] 表示第 i 名工人的工作质量，其最低期望工资为 wage[i] 。
+// 现在我们想雇佣 k 名工人组成一个工资组。在雇佣 一组 k 名工人时，
+// 我们必须按照下述规则向他们支付工资：
+// 对工资组中的每名工人，应当按其工作质量与同组其他工人的工作质量的比例来支付工资。
+// 工资组中的每名工人至少应当得到他们的最低期望工资。
+// 测试链接 : https://leetcode.cn/problems/minimum-cost-to-hire-k-workers/
+public class Code03_MinimumCostToHireKWorkers {
+
+	public static class Employee {
+		public double loserDegree;
+		public int quality;
+
+		public Employee(int w, int q) {
+			loserDegree = (double) w / (double) q;
+			quality = q;
+		}
+	}
+
+	public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+		int n = quality.length;
+		Employee[] employees = new Employee[n];
+		for (int i = 0; i < n; i++) {
+			employees[i] = new Employee(wage[i], quality[i]);
+		}
+		Arrays.sort(employees, (a, b) -> a.loserDegree <= b.loserDegree ? -1 : 1);
+		PriorityQueue<Integer> minTops = new PriorityQueue<Integer>((a, b) -> b - a);
+		double ans = Double.MAX_VALUE;
+		for (int i = 0, qualitySum = 0; i < n; i++) {
+			int curQuality = employees[i].quality;
+			if (minTops.size() < k) {
+				qualitySum += curQuality;
+				minTops.add(curQuality);
+				if (minTops.size() == k) {
+					ans = Math.min(ans, qualitySum * employees[i].loserDegree);
+				}
+			} else {
+				if (minTops.peek() > curQuality) {
+					qualitySum += curQuality - minTops.poll();
+					minTops.add(curQuality);
+				}
+				ans = Math.min(ans, qualitySum * employees[i].loserDegree);
+			}
+		}
+		return ans;
+	}
+
+}
