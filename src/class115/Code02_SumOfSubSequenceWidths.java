00bbc820adf1b5f3467d9a2ff652d1cf0a92f122
algorithmzuo <algorithmzuo@163.com>
2022-12-27 13:27:08 +0800

    modify code

---
+++ src/class115/Code02_SumOfSubSequenceWidths.java
@@ -0,0 +1,32 @@
+package class115;
+
+import java.util.Arrays;
+
+// 一个序列的 宽度 定义为该序列中最大元素和最小元素的差值。
+// 给你一个整数数组 nums ，返回 nums 的所有非空 子序列 的 宽度之和
+// 由于答案可能非常大，请返回对 109 + 7 取余 后的结果。
+// 子序列 定义为从一个数组里删除一些（或者不删除）元素，
+// 但不改变剩下元素的顺序得到的数组
+// 例如，[3,6,2,7] 就是数组 [0,3,1,6,2,2,7] 的一个子序列。
+// 测试链接 : https://leetcode.cn/problems/sum-of-subsequence-widths/
+public class Code02_SumOfSubSequenceWidths {
+
+	public static int sumSubseqWidths(int[] nums) {
+		Arrays.sort(nums);
+		int mod = 1000000007;
+		long ans = 0;
+		long A = 0;
+		long B = 0;
+		long C = 1;
+		long D = C;
+		for (int i = 1; i < nums.length; i++) {
+			A = (D * nums[i]) % mod;
+			B = (B * 2 + nums[i - 1]) % mod;
+			ans = (ans + A - B + mod) % mod;
+			C = (C * 2) % mod;
+			D = (D + C) % mod;
+		}
+		return (int) (ans);
+	}
+
+}
---
+++ src/class115/Code03_SumOfDistancesInTree.java
@@ -0,0 +1,70 @@
+package class115;
+
+import java.util.ArrayList;
+
+// 给定一个无向、连通的树
+// 树中有 n 个标记为 0...n-1 的节点以及 n-1 条边 。
+// 给定整数 n 和数组 edges ，
+// edges[i] = [ai, bi]表示树中的节点 ai 和 bi 之间有一条边。
+// 返回长度为 n 的数组 answer ，其中 answer[i] : 
+// 树中第 i 个节点与所有其他节点之间的距离之和。
+// 测试链接 : https://leetcode.cn/problems/sum-of-distances-in-tree/
+public class Code03_SumOfDistancesInTree {
+
+	public int N = 30001;
+	public int[] size = new int[N];
+	public int[] distance = new int[N];
+
+	// edges : {3,1}、{1,2}、{3,0}
+
+	// 0 : {3}
+	// 2 : {1}
+	// 3 : {1,0}
+	// 1 : {3,2}
+	public int[] sumOfDistancesInTree(int n, int[][] edges) {
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			graph.add(new ArrayList<>());
+		}
+		for (int[] edge : edges) {
+			graph.get(edge[0]).add(edge[1]);
+			graph.get(edge[1]).add(edge[0]);
+		}
+		collect(0, -1, graph);
+		int[] ans = new int[n];
+		setAns(0, -1, 0, graph, ans);
+		return ans;
+	}
+
+	// cur : 当前节点号
+	// father : 当前节点的父节点
+	// graph : 图
+	public void collect(int cur, int father, ArrayList<ArrayList<Integer>> graph) {
+		size[cur] = 1;
+		distance[cur] = 0;
+		// 当前节点 7
+		// graph 7 -> {4, 8 , 13}
+		for (int next : graph.get(cur)) {
+			if (next != father) {
+				collect(next, cur, graph);
+				distance[cur] += distance[next] + size[next];
+				size[cur] += size[next];
+			}
+		}
+	}
+
+	// cur -> 当前节点！
+	// father -> 当前节点的父
+	// upDistance -> 父亲扔给cur的第二部分距离！
+	// graph -> 图
+	// int[] ans 答案填写进ans
+	public void setAns(int cur, int father, int upDistance, ArrayList<ArrayList<Integer>> graph, int[] ans) {
+		ans[cur] = distance[cur] + upDistance;
+		for (int next : graph.get(cur)) {
+			if (next != father) {
+				setAns(next, cur, ans[cur] - distance[next] + size[0] - (size[next] << 1), graph, ans);
+			}
+		}
+	}
+
+}
