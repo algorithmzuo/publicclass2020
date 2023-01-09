fcf4cff978605e367fd3c40b54454715d1434308
algorithmzuo <algorithmzuo@163.com>
2023-01-09 12:40:27 +0800

    modify code

--- src/class118/Code01_ExpressionCompute.java
+++ src/class118/Code01_ExpressionCompute.java
@@ -1,9 +1,9 @@
-package class117;
+package class118;
 
 import java.util.LinkedList;
 
 // 本题测试链接 : https://leetcode.com/problems/basic-calculator-iii/
-public class Code08_ExpressionCompute {
+public class Code01_ExpressionCompute {
 
 	public static int calculate(String str) {
 		return f(str.toCharArray(), 0)[0];
--- src/class118/Code03_RegularExpressionMatch.java
+++ src/class118/Code03_RegularExpressionMatch.java
@@ -1,7 +1,7 @@
-package class117;
+package class118;
 
 // 测试链接 : https://leetcode.com/problems/regular-expression-matching/
-public class Code09_RegularExpressionMatch {
+public class Code03_RegularExpressionMatch {
 
 	// 暴力递归方法
 	public static boolean isMatch1(String str, String exp) {
