411ccdd63287550a8d9cef3c24140efe5dc745a0
algorithmzuo <algorithmzuo@163.com>
2023-01-12 14:59:15 +0800

    modify code

---
+++ src/class119/Code02_MaximumFrequencyStack.java
@@ -0,0 +1,54 @@
+package class119;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// 设计一个类似堆栈的数据结构，将元素推入堆栈，并从堆栈中弹出出现频率最高的元素。
+// 实现 FreqStack 类:
+// FreqStack() 构造一个空的堆栈。
+// void push(int val) 将一个整数 val 压入栈顶。
+// int pop() 删除并返回堆栈中出现频率最高的元素。
+// 如果出现频率最高的元素不只一个，则移除并返回最接近栈顶的元素。
+// 测试链接 : https://leetcode.cn/problems/maximum-frequency-stack/
+public class Code02_MaximumFrequencyStack {
+
+	class FreqStack {
+
+		// 出现的最大次数
+		private int topTimes;
+		// 每层节点
+		private HashMap<Integer, ArrayList<Integer>> cntValues = new HashMap<>();
+		// 每一个数出现了几次
+		private HashMap<Integer, Integer> valueTopTime = new HashMap<>();
+
+		public void push(int val) {
+			// 当前数词频+1
+			valueTopTime.put(val, valueTopTime.getOrDefault(val, 0) + 1);
+			// 当前数是什么词频 5 7次
+			int curTopTimes = valueTopTime.get(val);
+			if (!cntValues.containsKey(curTopTimes)) {
+				cntValues.put(curTopTimes, new ArrayList<>());
+			}
+			ArrayList<Integer> curTimeValues = cntValues.get(curTopTimes);
+			curTimeValues.add(val);
+			topTimes = Math.max(topTimes, curTopTimes);
+		}
+
+		public int pop() {
+			// 最大词频的那一层的链表(动态数组)
+			ArrayList<Integer> topTimeValues = cntValues.get(topTimes);
+			int ans = topTimeValues.remove(topTimeValues.size() - 1);
+			if (topTimeValues.size() == 0) {
+				cntValues.remove(topTimes--);
+			}
+			int times = valueTopTime.get(ans);
+			if (times == 1) {
+				valueTopTime.remove(ans);
+			} else {
+				valueTopTime.put(ans, times - 1);
+			}
+			return ans;
+		}
+	}
+
+}
---
+++ src/class119/Code04_LongestConsecutive.java
@@ -0,0 +1,65 @@
+package class119;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+// 本题测试链接 : https://leetcode.com/problems/longest-consecutive-sequence/
+public class Code04_LongestConsecutive {
+
+	// 课上讲的解法
+	public static int longestConsecutive(int[] nums) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int len = 0;
+		for (int num : nums) {
+			if (!map.containsKey(num)) {
+				map.put(num, 1);
+				int preLen = map.containsKey(num - 1) ? map.get(num - 1) : 0;
+				int posLen = map.containsKey(num + 1) ? map.get(num + 1) : 0;
+				int all = preLen + posLen + 1;
+				map.put(num - preLen, all);
+				map.put(num + posLen, all);
+				len = Math.max(len, all);
+			}
+		}
+		return len;
+	}
+
+	// 补充一个两张表：头表、尾表。非常好理解的方法
+	// 不是最优解，但是好理解
+	public static int longestConsecutive2(int[] nums) {
+		HashMap<Integer, Integer> headMap = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> tailMap = new HashMap<Integer, Integer>();
+		HashSet<Integer> visited = new HashSet<>();
+		for (int num : nums) {
+			if (!visited.contains(num)) {
+				visited.add(num);
+				headMap.put(num, 1);
+				tailMap.put(num, 1);
+				if (tailMap.containsKey(num - 1)) {
+					int preLen = tailMap.get(num - 1);
+					int preHead = num - preLen;
+					headMap.put(preHead, preLen + 1);
+					tailMap.put(num, preLen + 1);
+					headMap.remove(num);
+					tailMap.remove(num - 1);
+				}
+				if (headMap.containsKey(num + 1)) {
+					int preLen = tailMap.get(num);
+					int preHead = num - preLen + 1;
+					int postLen = headMap.get(num + 1);
+					int postTail = num + postLen;
+					headMap.put(preHead, preLen + postLen);
+					tailMap.put(postTail, preLen + postLen);
+					headMap.remove(num + 1);
+					tailMap.remove(num);
+				}
+			}
+		}
+		int ans = 0;
+		for (int len : headMap.values()) {
+			ans = Math.max(ans, len);
+		}
+		return ans;
+	}
+
+}
