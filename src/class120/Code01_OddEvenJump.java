49fd194457c284153d3a82ee590e3a1cbae95a4e
algorithmzuo <algorithmzuo@163.com>
2023-02-01 17:43:40 +0800

    modify code

--- src/class120/Code01_OddEvenJump.java
+++ src/class120/Code01_OddEvenJump.java
@@ -20,22 +20,76 @@ import java.util.TreeMap;
 // 测试链接 : https://leetcode.cn/problems/odd-even-jump/
 public class Code01_OddEvenJump {
 
+	public static void main(String[] args) {
+		// 在有序表里，key是有序组织的
+		TreeMap<Integer, String> orderMap = new TreeMap<>();
+		// 有序表中，以下所有操作
+		// 时间复杂度O(logN)！
+		// 非常快的！
+		orderMap.put(5, "我是5");
+		orderMap.put(10, "我是10");
+		orderMap.put(15, "我是15");
+		orderMap.put(-5, "我是-5");
+		orderMap.put(-15, "我是-15");
+		orderMap.put(50, "我是50");
+		
+		int num = 40;
+		// >= 40 记录
+		System.out.println(orderMap.ceilingKey(num));
+		
+
+//		System.out.println(orderMap.firstKey());
+//		
+//		System.out.println(orderMap.lastKey());
+//		
+//		int num = 7;
+//		// <= 7 且尽量大的key是多少
+//		System.out.println(orderMap.floorKey(num));
+//		// >= 7 且尽量小的key是多少
+//		System.out.println(orderMap.ceilingKey(num));
+//		
+//		System.out.println(orderMap.containsKey(5));
+//		
+//		System.out.println(orderMap.containsKey(7));
+		
+
+	}
+
 	public static int oddEvenJumps(int[] arr) {
+		
+		//       ? ? ?
+		// arr[0....n-1]
+		
+		
 		int n = arr.length;
+		// odd[i] : 如果来到i位置，处在奇数规则下，该去哪个位置！
 		int[] odd = new int[n];
+		// odd[i] : 如果来到i位置，处在偶数规则下，该去哪个位置！
 		int[] even = new int[n];
+		// 准备有序表！
+		// key : 值！
+		// value : 这个值出现的最左下标！
 		TreeMap<Integer, Integer> orderMap = new TreeMap<>();
 		for (int i = n - 1; i >= 0; i--) {
+			// 从右往左遍历
+			// 右边，有没有 >= arr[i]的值
 			Integer to = orderMap.ceilingKey(arr[i]);
 			odd[i] = to == null ? -1 : orderMap.get(to);
+			// 右边，有没有 <= arr[i]的值
 			to = orderMap.floorKey(arr[i]);
 			even[i] = to == null ? -1 : orderMap.get(to);
 			orderMap.put(arr[i], i);
 		}
+		
+		// dp[i][0] : 如果来到i位置，并且是奇数规则，能不能去往数组最终位置
+		// dp[i][1] : 如果来到i位置，并且是偶数规则，能不能去往数组最终位置
 		boolean[][] dp = new boolean[n][2];
 		dp[n - 1][0] = true;
 		dp[n - 1][1] = true;
-		int ans = 1;
+		// dp[n-2][0] dp[n-2][1]
+		// dp[n-3][0] dp[n-3][1]
+		// dp[n-4][0] dp[n-4][1]
+		int ans = 1; // n-1位置一定良好的！ans = 1
 		for (int i = n - 2; i >= 0; i--) {
 			dp[i][0] = odd[i] != -1 && dp[odd[i]][1];
 			dp[i][1] = even[i] != -1 && dp[even[i]][0];
--- src/class120/Code02_RegularExpressionMatch.java
+++ src/class120/Code02_RegularExpressionMatch.java
@@ -7,6 +7,66 @@ package class120;
 // 测试链接 : https://leetcode.cn/problems/regular-expression-matching/
 public class Code02_RegularExpressionMatch {
 
+	public static boolean isMatch(String str, String exp) {
+		char[] s = str.toCharArray();
+		char[] e = exp.toCharArray();
+		return zuo(s, e, 0, 0);
+	}
+
+	// 原始串，s
+	// 匹配串，e
+	// s[0.....n-1]
+	// e[0.....m-1]
+	// s[0....n-1]整体 ， 能不能， 被e[0.....m-1]的整体完全变出来！
+	// s[si.........]这个部分，能不能，被e[ei........]这个部分变出来
+	// 能变，true
+	// 不能，false
+	public static boolean zuo(char[] s, char[] e, int si, int ei) {
+		if (si == s.length && ei == e.length) {
+			return true;
+		}
+		// 不都为空
+
+		if (ei == e.length) {
+			// 原始串：还有！
+			// 匹配串：没了！
+			return false;
+		}
+
+		if (si == s.length) {
+			// 原始串：没了！
+			// 匹配串：还有！
+			// 关心，e[ei....] ：X*X*X*这种模式！false true
+			if (ei + 1 < e.length && e[ei + 1] == '*') {
+				// s : 没了
+				// e : . * . . . .
+				// ei ei+1
+				return zuo(s, e, si, ei + 2);
+			} else {
+				return false;
+			}
+		}
+
+		// s : 有字符
+		// e : 也有字符
+		// 普遍情况
+		// 1) e[ei + 1] 不是*这种情况！
+		boolean ans = false;
+		if (ei + 1 == e.length || e[ei + 1] != '*') {
+			ans = (s[si] == e[ei] || e[ei] == '.') && zuo(s, e, si + 1, ei + 1);
+		} else { // 2) e[ei+1] 是*
+			// a) 就是让【ei ei+1】变空
+			boolean a = zuo(s, e, si, ei + 2);
+			// b) 当前s[si]，能被e[ei + (ei+1)]搞定，才有后续
+			boolean b = false;
+			if (s[si] == e[ei] || e[ei] == '.') {
+				b = zuo(s, e, si + 1, ei);
+			}
+			ans = a || b;
+		}
+		return ans;
+	}
+
 	// 暴力递归方法
 	public static boolean isMatch1(String str, String exp) {
 		char[] s = str.toCharArray();
