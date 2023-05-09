7dbea0700b891755ae655b984514e30487ba5c1c
algorithmzuo <algorithmzuo@163.com>
2023-05-09 17:02:55 +0800

    modify code

---
+++ src/class133/Code01_RangesHasDominateNumber.java
@@ -0,0 +1,107 @@
+package class133;
+
+import java.util.HashMap;
+
+// 来自小红书
+// 小A认为如果在数组中有一个数出现了至少k次
+// 且这个数是该数组的众数，即出现次数最多的数之一
+// 那么这个数组被该数所支配
+// 显然当k比较大的时候，有些数组不被任何数所支配
+// 现在小A拥有一个长度为n的数组，她想知道内部有多少个区间是被某个数支配的
+// 2 <= k <= n <= 100000
+// 1 <= 数组的值 <= n
+public class Code01_RangesHasDominateNumber {
+
+	// 暴力方法
+	// 为了验证
+	// 时间复杂度O(N^3)
+	public static int dominates1(int[] arr, int k) {
+		int n = arr.length;
+		int ans = 0;
+		for (int l = 0; l < n; l++) {
+			for (int r = l; r < n; r++) {
+				if (ok(arr, l, r, k)) {
+					ans++;
+				}
+			}
+		}
+		return ans;
+	}
+
+	public static boolean ok(int[] arr, int l, int r, int k) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = l; i <= r; i++) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+		}
+		for (int times : map.values()) {
+			if (times >= k) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// 正式方法
+	// 时间复杂度O(N)
+	public static int dominates2(int[] arr, int k) {
+		int n = arr.length;
+		// 总数量
+		int all = n * (n + 1) / 2;
+		// 不被支配的区间数量
+		int except = 0;
+		// 次数表
+		// 0 : 0
+		// 1 : 0
+		// 2 : 0
+		int[] cnt = new int[n + 1];
+		// l ... r
+		// 窗口用这个形式[l,r)
+		// l...r-1 r(x)
+		// l == 0 r == 0 [l,r) 一个数也没有
+		// l == 0 r == 1 [0..0]
+		for (int l = 0, r = 0; l < n; l++) {
+			// [r] 即将要进来的
+			// cnt[arr[r]] + 1 < k
+			while (r < n && cnt[arr[r]] + 1 < k) {
+				// cnt[arr[r]]++;
+				// r++
+				cnt[arr[r++]]++;
+			}
+			// l..l
+			// l..l+1
+			// l..l+2
+			// l..r-1
+			except += r - l;
+			cnt[arr[l]]--;
+		}
+		return all - except;
+	}
+
+	// 为了测试
+	public static int[] randomArray(int n) {
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[i] = (int) (Math.random() * n) + 1;
+		}
+		return ans;
+	}
+
+	// 为了测试
+	public static void main(String[] args) {
+		int N = 100;
+		int testTimes = 5000;
+		System.out.println("测试开始");
+		for (int i = 0; i < testTimes; i++) {
+			int n = (int) (Math.random() * N) + 1;
+			int[] arr = randomArray(n);
+			int k = (int) (Math.random() * n) + 1;
+			int ans1 = dominates1(arr, k);
+			int ans2 = dominates2(arr, k);
+			if (ans1 != ans2) {
+				System.out.println("出错了!");
+			}
+		}
+		System.out.println("测试结束");
+	}
+
+}
---
+++ src/class133/Code03_MakeSumDivisibleByP.java
@@ -0,0 +1,45 @@
+package class133;
+
+import java.util.HashMap;
+
+// 来自谷歌、亚马逊、Bloomberg、字节跳动、苹果
+// 给你一个正整数数组 nums，请你移除 最短 子数组（可以为 空）
+// 使得剩余元素的 和 能被 p 整除。 不允许 将整个数组都移除。
+// 请你返回你需要移除的最短子数组的长度，如果无法满足题目要求，返回 -1 。
+// 子数组 定义为原数组中连续的一组元素。
+// 测试链接 : https://leetcode.cn/problems/make-sum-divisible-by-p/
+public class Code03_MakeSumDivisibleByP {
+
+	// 核心 : find = (curMod - allMod + p) % p;
+	public int minSubarray(int[] nums, int p) {
+		int n = nums.length;
+		int allMod = 0;
+		for (int num : nums) {
+			allMod = (allMod + num) % p;
+		}
+		if (allMod == 0) {
+			return 0;
+		}
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(0, -1);
+		int ans = Integer.MAX_VALUE;
+		int curMod = 0, find;
+		for (int i = 0; i < n; i++) {
+			// 0...i 累加和的余数
+			curMod = (curMod + nums[i]) % p;
+			// 如果p = 7，整体余数2，当前余数5，那么找之前的部分余数是3
+			// 如果p = 7，整体余数2，当前余数1，那么找之前的部分余数是6
+			// 整体变成下面的公式，可以自己带入各种情况验证
+			find = (curMod - allMod + p) % p;
+			if (map.containsKey(find)) {
+				if (i != n - 1 || map.get(find) != -1) {
+					// 防止删掉整体
+					ans = Math.min(ans, i - map.get(find));
+				}
+			}
+			map.put(curMod, i);
+		}
+		return ans == Integer.MAX_VALUE ? -1 : ans;
+	}
+
+}
