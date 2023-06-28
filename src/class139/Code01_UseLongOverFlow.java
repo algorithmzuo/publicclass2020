e045530ff829cb372718b315fe8f0f7a8cbb7b10
algorithmzuo <algorithmzuo@163.com>
2023-06-28 14:45:57 +0800

    modify code

---
+++ src/class139/Code01_UseLongOverFlow.java
@@ -0,0 +1,24 @@
+package class139;
+
+import java.math.BigInteger;
+
+public class Code01_UseLongOverFlow {
+
+	public static void main(String[] args) {
+		BigInteger z = new BigInteger("3000000000000000000000000000000000000000000");
+		System.out.println(z.longValue());
+
+		BigInteger a = new BigInteger("1000000000000000000000000000000000000000000");
+		BigInteger b = new BigInteger("2000000000000000000000000000000000000000000");
+		System.out.println(a.longValue() + b.longValue());
+
+		BigInteger c = new BigInteger("5000000000000000000000000000000000000000000");
+		BigInteger d = new BigInteger("2000000000000000000000000000000000000000000");
+		System.out.println(c.longValue() - d.longValue());
+
+		BigInteger e = new BigInteger("3000000000000000000000");
+		BigInteger f = new BigInteger("1000000000000000000000");
+		System.out.println(e.longValue() * f.longValue());
+	}
+
+}
