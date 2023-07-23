aa40d6ecec0647acf7adf5d212bb6e985ccdd10c
algorithmzuo <algorithmzuo@163.com>
2023-07-23 17:29:59 +0800

    modify code

--- src/class143/Code03_SplitArrayLargestSum.java
+++ src/class143/Code03_SplitArrayLargestSum.java
@@ -1,5 +1,8 @@
 package class143;
 
+// 给定一个非负整数数组 nums 和一个整数 m
+// 你需要将这个数组分成 m 个非空的连续子数组
+// 设计一个算法使得这 m 个子数组各自和的最大值最小
 // 测试链接：https://leetcode.cn/problems/split-array-largest-sum/
 public class Code03_SplitArrayLargestSum {
 
--- src/class143/Code06_FindKthSmallestPairDistance.java
+++ src/class143/Code06_FindKthSmallestPairDistance.java
@@ -2,6 +2,10 @@ package class143;
 
 import java.util.Arrays;
 
+// 数对 (a,b) 由整数 a 和 b 组成，其数对距离定义为 a 和 b 的绝对差值
+// 给你一个整数数组 nums 和一个整数 k
+// 数对由 nums[i] 和 nums[j] 组成且满足 0 <= i < j < nums.length
+// 返回 所有数对距离中 第 k 小的数对距离
 // 测试链接 : https://leetcode.cn/problems/find-k-th-smallest-pair-distance/
 public class Code06_FindKthSmallestPairDistance {
 
